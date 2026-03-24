7a4d0b6519f1bcd87514c61d122e0a9697d87dc3
Wiggo <toni.stark20148@gmail.com>
2026-03-25 01:48:12 +0700

    изменил обработку ошибок

--- 1/internal/handler/get.go
+++ 1/internal/handler/get.go
@@ -13,22 +13,22 @@ import (
 func (h *Handler) GetNotificationStatus(ctx *ginext.Context) {
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		zlog.Logger.Error().Msgf("ошибка обработки id: %v\n", err)
+		zlog.Logger.Error().Msgf("ошибка обработки id: %v", err)
 		ctx.JSON(http.StatusBadRequest, ginext.H{"err": "ID должен быть числом"})
 		return
 	}
 
 	status, err := h.srvc.GetNotificationStatus(id)
 	if err != nil {
-		zlog.Logger.Error().Msgf("ошибка получения статуса уведомления: %v\n", err)
+		zlog.Logger.Error().Msgf("ошибка получения статуса уведомления: %v", err)
 		if errors.Is(err, repository.ErrNoNotification) {
 			ctx.JSON(http.StatusBadRequest, ginext.H{
-				"err": err.Error(),
+				"err": err,
 			})
 			return
 		}
 		ctx.JSON(http.StatusInternalServerError, ginext.H{
-			"err": "внутренняя ошибка сервера: " + err.Error(),
+			"err": "внутренняя ошибка сервера",
 		})
 		return
 	}
