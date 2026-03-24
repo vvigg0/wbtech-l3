4a28a812aaaea6830111b8087914ae1b2859c7b6
Wiggo <toni.stark20148@gmail.com>
2026-03-25 01:54:42 +0700

    удаление из БД по ID

--- 1/internal/repository/delete.go
+++ 1/internal/repository/delete.go
@@ -19,3 +19,22 @@ func (r *Repository) UpdateNotificationStatus(id int, newStatus string) error {
 
 	return nil
 }
+
+func (r *Repository) DeleteNotification(id int) error {
+	query := "DELETE FROM notifications WHERE id=$1"
+
+	res, err := r.Master.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	aff, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if aff == 0 {
+		return ErrNoNotification
+	}
+
+	return nil
+}
